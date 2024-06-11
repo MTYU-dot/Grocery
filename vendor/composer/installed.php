757529bea2921a8c5982cf64cf26952ec0def924
Luki Fiagnia Saputa <lukifiaqnia@gmail.com>
2024-06-11 11:35:39 +0700

    perbuahan

---
+++ vendor/composer/installed.php
@@ -0,0 +1,29 @@
+<?php return array(
+    'root' => array(
+        'name' => 'twbs/bootstrap',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'ccfb308eabd3f0de0fe332da9aaaee23ae34a2de',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'twbs/bootstrap' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'ccfb308eabd3f0de0fe332da9aaaee23ae34a2de',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'twitter/bootstrap' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => 'dev-main',
+            ),
+        ),
+    ),
+);
